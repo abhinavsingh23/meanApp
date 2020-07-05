11b922ae5fa95cda1986b135eb478990b6f7fc5b
abhiavsingh2902@gmail.com <abhinavsingh2902@gmail.com>
2020-07-05 19:50:44 +0530

    Adding more components

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-
   templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  newPost = '';
+  enteredValue = '';
+  newPost = 'No Content';
 
-  onAddPost() {
-    this.newPost = 'the Users Post';
+  onAddPost(enteredValue) {
+    this.newPost = enteredValue;
   }
 }
